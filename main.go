8b7ed42dbb321410392550dde24f76b7b53e80e7
Krisztián  Gödrei <godreikrisztian@gmail.com>
2018-04-07 16:32:56 +0200

    update

--- main.go
+++ main.go
@@ -1,81 +1,71 @@
 package main
 
 import (
-	"fmt"
 	"os"
-	"path/filepath"
+	"os/exec"
 	"strings"
-
-	"github.com/ryanuber/go-glob"
 
 	"github.com/bitrise-io/go-utils/command"
 	"github.com/bitrise-io/go-utils/log"
 	"github.com/bitrise-tools/go-steputils/stepconf"
+	"github.com/godrei/steps-golint/gotool"
 )
 
-type config struct {
-	Include string `env:"include"`
+// Config ...
+type Config struct {
 	Exclude string `env:"exclude"`
 }
 
-func listFiles(dir, include, exclude string) ([]string, error) {
-	var files []string
-	return files, filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
+func installedInPath(name string) bool {
+	cmd := exec.Command("which", name)
+	outBytes, err := cmd.Output()
+	return err == nil && strings.TrimSpace(string(outBytes)) != ""
+}
 
-		if filepath.Ext(path) != ".go" {
-			return nil
-		}
-
-		if include != "" && !glob.Glob(include, path) {
-			return nil
-		}
-
-		if exclude != "" && glob.Glob(exclude, path) {
-			return nil
-		}
-
-		files = append(files, path)
-
-		return nil
-	})
+func failf(format string, args ...interface{}) {
+	log.Errorf(format, args...)
+	os.Exit(1)
 }
 
 func main() {
-	var cfg config
+	var cfg Config
 	if err := stepconf.Parse(&cfg); err != nil {
-		log.Errorf("Error: %s\n", err)
-		os.Exit(1)
+		failf("Error: %s\n", err)
 	}
 	stepconf.Print(cfg)
 
-	installCmd := command.New("go", "get", "-u", "-v", "github.com/kisielk/errcheck")
-	if err := installCmd.Run(); err != nil {
-		log.Errorf("Failed to install errcheck: %s", err)
-		os.Exit(1)
+	if !installedInPath("errcheck") {
+		cmd := command.New("go", "get", "-u", "github.com/kisielk/errcheck")
+
+		log.Infof("\nInstalling errcheck")
+		log.Donef("$ %s", cmd.PrintableCommandArgs())
+
+		if out, err := cmd.RunAndReturnTrimmedCombinedOutput(); err != nil {
+			failf("Failed to install errcheck: %s", out)
+		}
 	}
 
 	dir, err := os.Getwd()
 	if err != nil {
-		log.Errorf("Failed to get working directory: %s", err)
-		os.Exit(1)
+		failf("Failed to get working directory: %s", err)
 	}
 
-	files, err := listFiles(dir, cfg.Include, cfg.Exclude)
+	excludes := strings.Split(cfg.Exclude, ",")
+
+	packages, err := gotool.ListPackages(dir, excludes...)
 	if err != nil {
-		log.Errorf("Failed to list files: %s", err)
-		os.Exit(1)
+		failf("Failed to list packages: %s", err)
 	}
 
-	errcheckCmd := command.NewWithStandardOuts("errcheck", "-asserts=true", "-blank=true", "-verbose", strings.Join(files, "\n"))
+	log.Infof("\nRunning errcheck...")
 
-	fmt.Println()
-	log.Infof("$ %s", errcheckCmd.PrintableCommandArgs())
+	for _, p := range packages {
+		cmd := command.NewWithStandardOuts("errcheck", "-asserts=true", "-blank=true", "-verbose", p)
 
-	if err := errcheckCmd.Run(); err != nil {
-		log.Errorf("errcheck failed: %s", err)
-		os.Exit(1)
+		log.Printf("$ %s", cmd.PrintableCommandArgs())
+
+		if err := cmd.Run(); err != nil {
+			failf("errcheck failed: %s", err)
+		}
 	}
 }
