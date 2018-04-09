757e2e5197936546891962bb49b8018c918c3c4f
Krisztián  Gödrei <godreikrisztian@gmail.com>
2018-04-09 22:18:20 +0200

    update

--- main.go
+++ main.go
@@ -8,12 +8,11 @@ import (
 	"github.com/bitrise-io/go-utils/command"
 	"github.com/bitrise-io/go-utils/log"
 	"github.com/bitrise-tools/go-steputils/stepconf"
-	"github.com/godrei/steps-golint/gotool"
 )
 
 // Config ...
 type Config struct {
-	Exclude string `env:"exclude"`
+	Packages string `env:"packages,required"`
 }
 
 func installedInPath(name string) bool {
@@ -45,17 +44,7 @@ func main() {
 		}
 	}
 
-	dir, err := os.Getwd()
-	if err != nil {
-		failf("Failed to get working directory: %s", err)
-	}
-
-	excludes := strings.Split(cfg.Exclude, ",")
-
-	packages, err := gotool.ListPackages(dir, excludes...)
-	if err != nil {
-		failf("Failed to list packages: %s", err)
-	}
+	packages := strings.Split(cfg.Packages, ",")
 
 	log.Infof("\nRunning errcheck...")
 
